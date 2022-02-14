46372440a43c6b7423ce9a2bd971363fc097ff2e
郝麒睿 <1580660181@qq.com>
2022-02-14 11:00:11 +0800

    chore add css moduels

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -71,8 +71,8 @@ const cssRegex = /\.css$/;
 const cssModuleRegex = /\.module\.css$/;
 const sassRegex = /\.(scss|sass)$/;
 const sassModuleRegex = /\.module\.(scss|sass)$/;
-// const lessRegex = /\.(scss|less)$/;
-// const lessModuleRegex = /\.module\.(scss|less)$/;
+const lessRegex = /\.(scss|less)$/;
+const lessModuleRegex = /\.module\.(scss|less)$/;
 
 const hasJsxRuntime = (() => {
   if (process.env.DISABLE_NEW_JSX_TRANSFORM === 'true') {
@@ -544,37 +544,37 @@ module.exports = function (webpackEnv) {
                 'sass-loader'
               ),
             },
-            // {
-            //   test: lessRegex,
-            //   exclude: lessModuleRegex,
-            //   use: getStyleLoaders(
-            //     {
-            //       importLoaders: 2,
-            //       sourceMap: isEnvProduction && shouldUseSourceMap,
-            //     },
-            //     'less-loader'
-            //   ),
-            //   // Don't consider CSS imports dead code even if the
-            //   // containing package claims to have no side effects.
-            //   // Remove this when webpack adds a warning or an error for this.
-            //   // See https://github.com/webpack/webpack/issues/6571
-            //   sideEffects: true,
-            // },
+            {
+              test: lessRegex,
+              exclude: lessModuleRegex,
+              use: getStyleLoaders(
+                {
+                  importLoaders: 2,
+                  sourceMap: isEnvProduction && shouldUseSourceMap,
+                },
+                'less-loader'
+              ),
+              // Don't consider CSS imports dead code even if the
+              // containing package claims to have no side effects.
+              // Remove this when webpack adds a warning or an error for this.
+              // See https://github.com/webpack/webpack/issues/6571
+              sideEffects: true,
+            },
             // Adds support for CSS Modules, but using SASS
             // using the extension .module.scss or .module.sass
-            // {
-            //   test: lessModuleRegex,
-            //   use: getStyleLoaders(
-            //     {
-            //       importLoaders: 2,
-            //       sourceMap: isEnvProduction && shouldUseSourceMap,
-            //       modules: {
-            //         getLocalIdent: getCSSModuleLocalIdent,
-            //       },
-            //     },
-            //     'less-loader'
-            //   ),
-            // },
+            {
+              test: lessModuleRegex,
+              use: getStyleLoaders(
+                {
+                  importLoaders: 2,
+                  sourceMap: isEnvProduction && shouldUseSourceMap,
+                  modules: {
+                    getLocalIdent: getCSSModuleLocalIdent,
+                  },
+                },
+                'less-loader'
+              ),
+            },
             // "file" loader makes sure those assets get served by WebpackDevServer.
             // When you `import` an asset, you get its (virtual) filename.
             // In production, they would get copied to the `build` folder.
--- src/pages/others/anomaly/404.tsx
+++ src/pages/others/anomaly/404.tsx
@@ -1,12 +1,11 @@
 import React from "react";
-import styles from "./404.less";
+import styles from "./404.module.less";
 import svg404 from "@assets/svg/404.svg";
 
 const page404 = () => {
-
-
+  
   return (
-    <div>
+    <div className={styles.imgDiv}>
       <img src={svg404} alt="" className={styles.img}/>
     </div>
   )
