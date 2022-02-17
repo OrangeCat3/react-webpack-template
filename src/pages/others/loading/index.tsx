4ccc688fe0f715d6eb9bc0b806422202fe4226bb
郝麒睿 <1580660181@qq.com>
2022-02-17 16:41:20 +0800

    feat:loading优化

--- src/pages/others/loading/index.tsx
+++ src/pages/others/loading/index.tsx
@@ -1,23 +1,25 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
+import { Spin } from 'antd';
 import styles from "./index.module.less";
 
 function Loading() {
   const { t } = useTranslation();
   return (
     <div className={styles.context}>
-      <div>
+      <div className={styles.spin}>
+        <Spin size="large"/>
+      </div>
+      <div className={styles.char}>
         <h1>{t("loading")}</h1>
       </div>
-      <div>
-        <Animation/>
-      </div>
+      <div className={styles.point}>
+        <h1> . . .</h1>
+      </div> 
     </div>
   );
 }
 
-function Animation() {
-  return <h1>...</h1>
-}
+
 
 export default Loading;
