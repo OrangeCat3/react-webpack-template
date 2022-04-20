f2981fa11de7ab928dceaeacae6c7d3dd0fdeb03
郝麒睿 <1580660181@qq.com>
2022-04-20 23:00:56 +0800

    feat 简化baseurl

--- src/services/request/config.ts
+++ src/services/request/config.ts
@@ -1,15 +1,10 @@
 let BASE_URL: string|undefined;
 
-if(process.env.REACT_APP_isMock) {
-  BASE_URL = process.env.REACT_APP_Mock_BaseURL;
-} else if(process.env.NODE_ENV === 'development' && !process.env.REACT_APP_isMock) {
+if(process.env.NODE_ENV === 'development') {
   BASE_URL = process.env.REACT_APP_DEV_BaseURL;
-} else if(process.env.NODE_ENV === 'production') {
-  BASE_URL = process.env.REACT_APP_PRO_BaseURL;
+} else {
+  BASE_URL = process.env.REACT_APP_PRO_BaseURL
 }
-
-console.log(BASE_URL);
-
 
 const TIMEOUT = 5000;
 
